863e8b9cda2ba9910306ff3b92b1a3b8cdf2e67c
EliasLindberg <lindberg.elias@elev.ga.ntig.se>
2023-02-07 14:18:10 +0100

    Java GUI

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        /*
+        JFrame frame = new JFrame(); //skapar en frame
+        frame.setTitle("Bra titel"); //sätter titel på frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //stänger applikationen
+        frame.setResizable(false); //förhindrar storleken att ändras på frame
+        frame.setSize(420,420); //sätter x och y dimensioner
+        frame.setVisible(true); //gör frame synlig
+
+        ImageIcon image = new ImageIcon("ikon.jpg"); //skapar en image icon
+        frame.setIconImage(image.getImage()); //ändrar framens ikon
+        frame.getContentPane().setBackground(new Color(157,64,200)); //ändrar bakgrundens färg
+        */
+
+        MyFrame myFrame = new MyFrame();
+
+
     }
 }
--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class MyFrame {
+import javax.swing.*;
+import java.awt.*;
+
+public class MyFrame extends JFrame {
+    
+    MyFrame() {
+        this.setTitle("Bra titel"); //sätter titel på this
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //stänger applikationen
+        this.setResizable(false); //förhindrar storleken att ändras på this
+        this.setSize(420,420); //sätter x och y dimensioner
+        this.setVisible(true); //gör this synlig
+
+        ImageIcon image = new ImageIcon("ikon.jpg"); //skapar en image icon
+        this.setIconImage(image.getImage()); //ändrar thisns ikon
+        this.getContentPane().setBackground(new Color(157,64,200)); //ändrar bakgrundens färg
+    }
+    
 }
